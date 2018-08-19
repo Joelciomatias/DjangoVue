0705e0ca3eb52831581e5c53c5f8a179a45e8eff
joelcio_psx@hotmail.com <chuknoia2015>
2018-08-19 14:46:20 -0300

    criada a rota homepage/games para receber o json

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from django.core.serializers import serialize
+from django.http import HttpResponse
 from homepage.models import Games
 
 def index(request):
     games = Games.objects.all()
     return render(request, "homepage/index.html", {'games': games})
+
+def games(request):
+    games = serialize("json", Games.objects.all())
+    return HttpResponse(games, content_type="application/json")
---
+++ static/js/components/Exemplo.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="">
+        <table class="table">
+        <thead>
+            <th>Titulo</th>
+            <th>Plataforma</th>
+        </thead>
+        <tbody>
+            
+            <tr v-for="game in lista" :key="game.id"> 
+                <td>{{ game.titulo }}</td>
+                <td>{{ game.plataformas }}</td>
+            </tr>
+            
+        </tbody>
+    </table>
+
+    </div>
+</template>
+<script>
+export default {
+    
+    data() {
+        return{
+            lista:[]
+        }
+    },
+    mounted(){
+        this.$http.get("homepage/games").then((req
+        ) => this.lista = req.data)
+
+    }
+}
+</script>
+<style lang="css">
+
+</style>
+
+
