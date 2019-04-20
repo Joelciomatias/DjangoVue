d21f297a91098063251d0574521391421d6ad78a
joelcio_psx@hotmail.com <chuknoia2015>
2019-04-20 14:23:15 -0300

    integrado com o vue.js

--- api_example/urls.py
+++ api_example/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/topics', views.names),
+    path('/topics/<region_id>/', views.topics),
+    path('/search/<query>/', views.search),
 ]
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div id="app">
-    <button @click="SelectWinner()">Select a Winner</button>
-    <p>
-      {{winner}}
-    </p>
+    <ul id="example-1">
+      <div v-for="item in topics">
+        <a :href="item.url" target="_blank">
+          <li>{{ item.name }}</li>
+        </a>
+      </div>
+    </ul>
+    <br/>
+    <input type="text" id="search-here">
+    <button @click="searchTwittes()">Search twittes</button>
+      <div v-for="item in twittes">
+            <img :alt="item.screen_name" :src="item.image_url"/>
+            <li>{{ item.screen_name +' - '+item.name}}</li>
+            <p>{{item.text}}</p>
+            <hr/>
+          <!-- </a> -->
+      </div>
   </div>
 </template>
 <script>
@@ -11,25 +24,36 @@ import axios from 'axios';
 export default {
   name: 'App',
   data() {
-      return {
-          names: null,
-          winner: null,
+    return {
+      items: [
+        { message: 'Algo' },
+        { message: 'Outro' }
+      ],
+        twittes:null,
+        topics:null
       }
   },
   mounted: function() {
-    this.FetchData();
+    this.getTrendingTopics();
   },
   methods: {
-      FetchData: function() {
-          var app = this;
-          axios.get(process.env.API_URL + "/api_example/").then(response => {
-              // app.names = response.data.names;
-              console.log(response.data);
-          });
+      getTrendingTopics: function() {
+        var app = this;
+        var topics = null;
+        axios.get(process.env.API_URL + "/api_example/topics/23424768/").then(response => {
+            topics = response.data.topics
+            topics = topics.slice(0,5)
+            console.log(topics);
+            this.topics = topics
+        });
       },
-      SelectWinner: function() {
-        var winner = this.names[Math.floor(Math.random()*this.names.length)];
-        this.winner = winner;
+      searchTwittes: function() {
+        var app = this;
+        var query = document.getElementById('search-here').value || 'brasil'
+        axios.get(process.env.API_URL + `/api_example/search/${query}/`).then(response => {
+            console.log(response.data.twittes);
+            this.twittes = response.data.twittes
+        });
       },
   }
 }
