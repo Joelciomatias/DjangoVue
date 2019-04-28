26f9c56d0e79aae4b5dc423e9dca4f5a8ce6805d
joelcio_psx@hotmail.com <chuknoia2015>
2019-04-28 15:37:41 -0300

    mais um tapa na home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,37 +1,44 @@
 <template>
   <div id="home">
     <b-row class="text-center">
-    <b-col></b-col>
-    <b-col cols="8">
-      <img id="logo" src="../../static/Twitter_logo_blue.png" width='' height=''  alt="Fluid-grow image"/>
-    </b-col>
-    <b-col></b-col>
-  </b-row class="text-center">
+      <b-col></b-col>
+      <b-col cols="8">
+        <img id="logo" src="../../static/Twitter_logo_blue.png" width='' height=''  alt="Fluid-grow image"/>
+      </b-col>
+      <b-col></b-col>
+    </b-row class="text-center">
     <div id=container>
-       <b-row>
-    <b-col></b-col>
-    <b-col>
-      <ul id="example-1">
-        <div v-for="item in topics">
-          <b-link :href="item.url" target="_blank">
-            <li>{{ item.name }}</li>
-          </b-link>
-        </div>
-      </ul>
-
-    </b-col>
-  </b-row>
+      <b-row>
+        <b-col>
+          <h4>Trending Now</h4>
+          <ul id="example-1">
+            <div v-for="item in topics">
+              <b-link :href="item.url" target="_blank">
+                <li>{{ item.name }}</li>
+              </b-link>
+            </div>
+          </ul>
+        </b-col>
+        <b-col>
+          <ul id="example-1">
+            <div v-for="item in topics2">
+              <b-link :href="item.url" target="_blank">
+                <li>{{ item.name }}</li>
+              </b-link>
+            </div>
+          </ul>
+        </b-col>
+      </b-row>
       <br/>
       <b-row class="text-center">
         <b-col cols="12">
-        <search></search>
+          <search></search>
         </b-col>
       </b-row>
     </div>
   </div>
 </template>
 <script>
-import axios from 'axios';
 import Search from './Search-results.vue'
 import { fetchTrendingTopics, fetchTwettsSearch } from '.././services/http'
 // import func from '../vue-temp/vue-editor-bridge';
@@ -44,7 +51,8 @@ export default {
     return {
         mainProps: { width: 500, height: 400, class: 'm1' },
         twittes:null,
-        topics:null
+        topics:null,
+        topics2:null
       }
   },
   mounted: function() {
@@ -55,6 +63,7 @@ export default {
       let regionId = 23424768
       fetchTrendingTopics(regionId).then(res => {
         this.topics = res.data.topics.slice(0,5)
+        this.topics2 = res.data.topics.slice(5,10)
         // console.log(this.topics);
       })
     },
@@ -63,13 +72,21 @@ export default {
 </script>
 <style>
 #home {
-  background-color: #F5F5F5
+  background-color: #F5F5F5;
+  padding: 50px 15px 15px 15px;
+  min-height: 50rem;
 }
 #container{
-
+padding-top: 20px;
 }
 #logo{
   /* margin-left: 40%; */
+}
+h4 {
+  color: #5E9FCA;
+}
+ul li {
+  list-style-type: none;
 }
 /* blue #5E9FCA */
 </style>
--- src/components/Search-results.vue
+++ src/components/Search-results.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="results">
-    <input type="text" id="search-here">
+    <!-- <input type="text" id="search-here"> -->
+    <!-- <mdbInput label="Username" icon="user" /> -->
+    <b-input-group>
+      <b-input-group-prepend>
+        <span class="input-group-text"><i src="../../static/Icon_search_big.png" 
+        class="fa fa-search fa-lg"></i></span>
+      </b-input-group-prepend>
+      <b-form-input id="search-here" class="LoginInput" size="lg" placeholder="Buscar Tweets">
+      </b-form-input>
+        <b-input-group-append>
+          <b-button @click="searchTwittes()" variant="info"><i class="fa fa-arrow-right"  aria-hidden="true"></i></b-button>
+        </b-input-group-append>
+    </b-input-group>
     <br/>
-    <button @click="searchTwittes()">Search twittes</button>
       <div v-for="item in twittes">
             <img :alt="item.screen_name" :src="item.image_url"/>
             <li>{{ item.screen_name +' - '+item.name}}</li>
@@ -12,11 +23,15 @@
   </div>
 </template>
 <script>
-import axios from 'axios';
+
+import { mdbInput } from 'mdbvue';
 import { fetchTrendingTopics, fetchTwettsSearch } from '.././services/http'
 // import func from '../vue-temp/vue-editor-bridge';
 export default {
   name: 'Search',
+  componens: {
+      mdbInput
+  },
   data() {
     return {
         mainProps: { width: 500, height: 400, class: 'm1' },
@@ -31,12 +46,24 @@ export default {
       let query = document.getElementById('search-here').value || 'brasil'
       fetchTwettsSearch(query).then(res => {
         this.twittes = res.data.twittes
-        // console.log(res.data.twittes);
+        console.log(res.data.twittes);
       })
     },
   }
 }
 </script>
 <style>
+.UniqueFullWidth .input-group-text {
+  width: 48px;
+  border-right: none;
+  background-color: #ffffff;
+}
+.UniqueFullWidth [class^="fa-"], [class*=" fa-"] {
+  display: inline-block;
+  width: 100%;
+}
+.UniqueFullWidth .LoginInput {
+  border-left: none;
+}
 
 </style>
